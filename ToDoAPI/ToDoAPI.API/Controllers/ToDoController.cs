53d710cae673232fa014afc549a1c9b9c702fe89
mindiford <mindiford@live.com>
2021-12-10 18:18:16 -0600

    ToDoController with all 5 actions tested in Postman

---
+++ ToDoAPI/ToDoAPI.API/Controllers/ToDoController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoAPI.API.Models; // Added for access to the DTO's
+using ToDoAPI.DATA.EF; // Access to the EF layer
+using System.Web.Http.Cors; // Added for access to modify the CORS funcitonality in this controller
+
+namespace ToDoAPI.API.Controllers
+{
+
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ToDoController : ApiController
+    {
+
+        ToDoEntities db = new ToDoEntities();
+
+        public IHttpActionResult GetToDo()
+        {
+            List<ToDoViewModel> toDo = db.TodoItems.Include("Category").Select(t => new ToDoViewModel()
+            {
+                TodoId = t.TodoId,
+                Action = t.Action,
+                Done = t.Done,
+                CategoryId = t.CategoryId//,
+                //Category = new CategoryViewModel()
+                //{
+                //    CategoryId = t.Category.CategoryId,
+                //    Name = t.Category.Name,
+                //    Description = t.Category.Description
+                //}
+
+            }).ToList();
+
+            if (toDo.Count == 0)
+            {
+                return NotFound(); // 404 Error
+            }
+
+            return Ok(toDo); // 200 sucess code and pass todo into the Ok response
+
+        } // End GetTodo()
+
+        // api/todo/id - Get Todo (READ)
+        public IHttpActionResult GetTodo(int id)
+        {
+            ToDoViewModel todo = db.TodoItems.Include("Category").Where(t => t.TodoId == id).Select(t => new ToDoViewModel()
+            {
+                TodoId = t.TodoId,
+                Action = t.Action,
+                Done = t.Done,
+                CategoryId = t.CategoryId
+            }).FirstOrDefault();
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return Ok(todo);
+        } // End GetTodo
+
+        // POST - Create functionality
+        // api/todo (HttpPost)
+        public IHttpActionResult PostTodo(ToDoViewModel todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Invalid Data");
+            }
+
+            TodoItem newTodoItem = new TodoItem()
+            {
+
+                Action = todo.Action,
+                Done = todo.Done,
+                CategoryId = todo.CategoryId
+            };
+
+            db.TodoItems.Add(newTodoItem);
+            db.SaveChanges();
+            return Ok(newTodoItem);
+        } // End PostTodo
+
+        // PUT = Edit
+        // api/todo (HttpPut)
+        public IHttpActionResult PutTodo(ToDoViewModel todo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            TodoItem existingTodo = db.TodoItems.Where(t => t.TodoId == todo.TodoId).FirstOrDefault();
+
+            if (existingTodo != null)
+            {
+                existingTodo.TodoId = todo.TodoId;
+                existingTodo.Action = todo.Action;
+                existingTodo.Done = todo.Done;
+                existingTodo.CategoryId = todo.CategoryId;
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        } // End PutTodo()
+
+        // api/todo/id
+        public IHttpActionResult DeleteTodo(int id)
+        {
+            TodoItem todo = db.TodoItems.Where(t => t.TodoId == id).FirstOrDefault();
+
+            if (todo != null)
+            {
+                db.TodoItems.Remove(todo);
+                db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+        } // End Delete
+
+        // dispose of any connections to db after we are done
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+    } // End class
+} // End namespace
---
+++ ToDoAPI/ToDoAPI.API/Models/ToDoModels.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using ToDoAPI.DATA.EF;
+
+namespace ToDoAPI.API.Models
+{
+    public class ToDoViewModel
+    {
+        [Key]
+        public int TodoId { get; set; }
+
+        [Required]
+        public string Action { get; set; }
+
+        [Required]
+        public bool Done { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+    }
+
+    public class CategoryViewModel
+    {
+
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = " ** Cannot exceed 50 characters")]
+        public string Name { get; set; }
+
+        [StringLength(100, ErrorMessage = " ** Cannot exceed 100 characters")]
+        public string Description { get; set; }
+
+    }
+}
